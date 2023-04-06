3fe1855c5a61c8224ac91391af978e6e8768edc4
Russel Torres <russtorres@gmail.com>
2023-04-06 11:32:53 -0700

    l1o: add leave-one-out analysis method

---
+++ coregister/process/leave_one_out_fit.py
@@ -0,0 +1,37 @@
+"""determine leave-one-out results of transformation"""
+import copy
+
+import numpy
+
+import coregister.transform.transform
+
+
+def leave_one_out_fitting(transform_dict, src_pts, dst_pts):
+    # copy may not be necessary
+    tform_dict = copy.deepcopy(transform_dict)
+
+    def fit_transform(src, dst):
+        tform = coregister.transform.Transform(json=tform_dict)
+        tform.estimate(src, dst)
+        return tform
+
+    predicted_dsts = numpy.empty_like(dst_pts)
+
+    for l1out_idx in range(src_pts.shape[0]):  # src_pts, dst_pts):
+        pts_mask = numpy.zeros(src_pts.shape[0], dtype=bool)
+        pts_mask[l1out_idx] = True
+
+        masked_src_pts = src_pts[~pts_mask]
+        masked_dst_pts = dst_pts[~pts_mask]
+
+        l1out_tform = fit_transform(masked_src_pts, masked_dst_pts)
+
+        l1out_src_pts = numpy.array([src_pts[l1out_idx]])
+
+        predicted_dst = l1out_tform.tform(l1out_src_pts)[0]
+
+        predicted_dsts[l1out_idx] = predicted_dst
+
+    residual = numpy.linalg.norm(dst_pts - predicted_dsts, axis=1)
+
+    return residual, predicted_dsts
