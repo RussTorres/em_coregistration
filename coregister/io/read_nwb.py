11621203cb3d266e21f8247bd788b63afb99e490
Russel Torres <russtorres@gmail.com>
2023-04-07 16:32:27 -0700

    io: add option to preprocess polygon and center when reading roi info

--- coregister/io/read_nwb.py
+++ coregister/io/read_nwb.py
@@ -1,10 +1,13 @@
 import dataclasses
 import os
 
-# import cv2
 import h5py
 import numpy
-# import shapely
+
+from coregister.process.roi import (
+    polygon_from_pix_mask,
+    center_from_pix_mask
+)
 
 
 def ldel(s, lead):
@@ -15,36 +18,6 @@ def ldel(s, lead):
     return s[todel:]
 
 
-# def roi_mask_to_contour_pts(mask_idx_array, mask_buffer=(0, 0)):
-#     # TODO phase out buffer -- unnecessary
-#     mask_buffer = numpy.array(mask_buffer)
-# 
-#     idx_offset = mask_idx_array.min(axis=1) - numpy.array(mask_buffer)
-#     mask_shape = mask_idx_array.ptp(axis=1) + 1
-# 
-#     zero_array = numpy.zeros(
-#         (mask_shape + (2 * numpy.array(mask_buffer)))[::-1],
-#         dtype="uint8")
-# 
-#     fill_idxs = (mask_idx_array.T - idx_offset).T
-# 
-#     zero_array[fill_idxs[1, :], fill_idxs[0, :]] = 1
-# 
-#     im, contours, hierarchy = cv2.findContours(
-#         zero_array, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-#     contour = contours[0]
-#     contour += idx_offset
-#     return contour.reshape(contour.shape[0], contour.shape[2])
-# 
-# 
-# def roi_mask_to_polygon(*args, add_z=None, **kwargs):
-#     contour_pts = roi_mask_to_contour_pts(*args, **kwargs)
-#     if add_z is not None:
-#         contour_pts = numpy.insert(
-#             contour_pts, contour_pts.shape[1], add_z, 1)
-#     return shapely.geometry.Polygon(contour_pts)
-
-
 @dataclasses.dataclass
 class FunctionalRoiInfo:
     # pulled from file
@@ -53,6 +26,7 @@ class FunctionalRoiInfo:
     plane_z: float 
     pix_mask: numpy.ndarray
     trace: numpy.ndarray
+    score: float
 
     # computed later
     polygon = None
@@ -61,6 +35,11 @@ class FunctionalRoiInfo:
     @property
     def name(self):
         return f"{self.plane_name}_{self.roi_name}"
+
+    def compute_values(self):
+        # TODO: some wasted calculation here can be more efficient
+        self.polygon = polygon_from_pix_mask(self.pix_mask)
+        self.center = center_from_pix_mask(self.pix_mask)
 
 
 class ROI_mask_nwb:
@@ -82,6 +61,10 @@ class ROI_mask_nwb:
     @property
     def processing(self):
         return self._h5_file["processing"]
+
+    @property
+    def analysis(self):
+        return self._h5_file["analysis"]
     
     @property
     def rois_and_traces(self):
@@ -115,34 +98,28 @@ class ROI_mask_nwb:
                 roi_name = self._group_name(roi)
                 yield plane_name, roi_name, roi
 
-    def yield_roi_info(self):
+    def yield_roi_info(self, compute_roi_info=False):
         for rat in self.rois_and_traces:
             traces = rat["DfOverF"]["dff_raw"]["data"][...]
             rat_name = self._group_name(rat)
             plane_name = rat_name.split("_")[-1]
             plane_z = self._plane_integer_from_name(plane_name)
+            pika_scores = self.analysis[f"roi_classification_pika/{plane_name}/score"][...]
 
             for i, roi in enumerate(self.get_roi_groups_from_plane_name(
                                         plane_name)):
                 roi_name = self._group_name(roi)
                 pix_mask = roi["pix_mask"][...]
                 trace = traces[i]
-                yield FunctionalRoiInfo(
+                pika_score = pika_scores[i]
+                roi_info = FunctionalRoiInfo(
                     plane_name=plane_name,
                     roi_name=roi_name,
                     plane_z=plane_z,
                     pix_mask=pix_mask,
                     trace=trace,
+                    score=pika_score
                     )
-
-    # def get_roi_plane_names_to_polygon(self, include_plane_z=True):
-    #     # TODO namedtuple as keys
-    #     plane_name_roi_name_to_polygon = {}
-    #     for plane_name, roi_name, roi in self.yield_plane_name_roi_name_roi_group_tups():
-    #         if include_plane_z:
-    #             plane_z = self._plane_integer_from_name(plane_name)
-    #         else:
-    #             plane_z = None
-    #         roi_polygon = roi_mask_to_polygon(roi["pix_mask"][:], add_z=plane_z)
-    #         plane_name_roi_name_to_polygon[(plane_name, roi_name)] = roi_polygon
-    #     return plane_name_roi_name_to_polygon
+                if compute_roi_info:
+                    roi_info.compute_values()
+                yield roi_info
